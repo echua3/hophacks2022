73f6e073d6154da010a917507d1d18858682562c
echua3 <epiphany.chua3@gmail.com>
2022-09-17 19:19:01 -0400

    reformat posts

--- client/src/components/comment/CommentFeed.js
+++ client/src/components/comment/CommentFeed.js
@@ -4,26 +4,25 @@ import Col from 'react-bootstrap/Col';
 import './style/CommentFeed.css';
 
 function CommentFeed() {
+  const posts = [
+    {username: 'Jane Doe', header: 'Header 1', comment: 'This post is super cool!'},
+    {username: 'John Smoe', header: 'Header 2', comment: 'Valid'},
+    {username: 'Joe Fawn', header: 'Header 3', comment: 'He is really saying something'}
+  ];
   return (
     <div class='horizontal-center'>
         <Col>
-            {[
-            'User 1',
-            'User 2',
-            'User 3',
-            'User 4',
-            'User 5',
-            'User 6',
-            'User 7',
-            'User 8',
-            ].map((variant) => (
+            {posts.map((post) => (
                 <ArticleComment 
-                    bg={variant.toLowerCase()}
-                    key={variant}
-                    className="mb-2"
-                > {variant}
+                  name = {post.username}
+                  header = {post.header} 
+                  comment = {post.comment}
+                  // bg={post.toLowerCase()}
+                  key={post}
+                  className="mb-2"
+                > {post}
                 </ArticleComment>
-        ))}
+        ))  }
         </Col>
     </div>
   );
