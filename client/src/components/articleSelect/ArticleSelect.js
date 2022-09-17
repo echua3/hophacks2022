a4306d5cf03a6211a905d96b82ce71b5642ea92e
lulugrubb <lgrubb3@jhu.edu>
2022-09-17 17:06:23 -0400

    ArticleSelect css

--- client/src/components/articleSelect/ArticleSelect.js
+++ client/src/components/articleSelect/ArticleSelect.js
@@ -4,9 +4,10 @@ import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 //import {Link} from 'react-router-dom';
+import './ArticleSelect.css';
 
 let url1 = "https://www.jhunewsletter.com/article/2022/09/sga-returns-to-discuss-welcome-event-for-all-students"
-let url2 = "https://hub.jhu.edu/2022/09/12/flu-vaccine-mandate-2022/"
+let url2 = "https://www.jhunewsletter.com/article/2022/09/rain-or-shine-mens-soccer-remains-unbeaten-after-weekend-doubleheader"
 let url3 = "https://www.jhunewsletter.com/article/2022/09/dont-get-cocky-blue-jays-rank-is-just-a-number"
 
 class ArticleSelect extends Component {
@@ -17,15 +18,16 @@ class ArticleSelect extends Component {
     render() {
         return (
             <div id="parent">
+                
                 <ButtonGroup>
                 <a href={"/view?url=" + url1} target="_blank">
-                    <Button> Article 1</Button>
+                    <Button className="artButton" name="artButton"> Article 1</Button>
                     </a>
                 <a href={"/view?url=" + url2} target="_blank">
-                    <Button> Article 2</Button>
+                    <Button className="artButton" name="artButton"> Article 2</Button>
                 </a>
                 <a href={"/view?url=" + url3} target="_blank">
-                    <Button> Article 3</Button>
+                    <Button className="artButton" name="artButton"> Article 3</Button>
                 </a>
                 </ButtonGroup>
             </div>
