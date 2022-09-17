25cee2408c433a8a720559a2d897dda5ed9c728c
echua3 <epiphany.chua3@gmail.com>
2022-09-17 10:58:49 -0400

    center comment feed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CommentFeed from './components/CommentFeed';
+import CommentFeed from './components/comment/CommentFeed';
 
 function App() {
   return (
---
+++ client/src/components/comment/ArticleComment.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+function ArticleComment() {
+  return (
+    <Card className="text-left" style={{ width: '18rem' }}>
+      <Card.Header>User</Card.Header>
+      <Card.Body>
+        <Card.Title>Comment</Card.Title>
+        <Card.Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam felis, elementum eu euismod sit amet, semper non diam. Mauris nec viverra eros. Mauris rhoncus, diam nec viverra malesuada, tortor ante egestas libero, quis iaculis diam nibh at quam. Donec sed nulla consectetur, maximus augue id, blandit sapien. Mauris condimentum.
+        </Card.Text>
+        <Button variant="primary">Go to article</Button>
+      </Card.Body>
+      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+    </Card>
+  )
+}
+
+export default ArticleComment
