7c8d3449fb68bec5e0bcaabdae47b5d7cbdbc510
echua3 <epiphany.chua3@gmail.com>
2022-09-17 12:04:44 -0400

    nav bar and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CommentFeed from './components/comment/CommentFeed';
+import Navbar from './components/navBar';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+import ArticleViewer  from './components/articleViewer/ArticleViewer'
 
 function App() {
   return (
-    <div>
-      <CommentFeed/>
-    </div>
+    <Router>
+    <Navbar />
+    <Routes>
+        {/* <Route exact path='/' exact element={<Home />} /> */}
+        <Route path='/articles' element={<ArticleViewer/>} />
+        <Route path='/posts' element={<CommentFeed/>} />    
+    </Routes>
+    </Router>
   );
 }
 
