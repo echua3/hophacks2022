1473dc13fd1a4e49eb9f75b201e08a3ce54952f5
TobBot2 <38795143+TobBot2@users.noreply.github.com>
2022-09-17 11:47:55 -0400

    added to ArticleViewer components

--- client/src/components/articleViewer/ArticleViewer.js
+++ client/src/components/articleViewer/ArticleViewer.js
@@ -1,11 +1,9 @@
 import './ArticleViewer.css';
 
-let articleURL = "https://getbootstrap.com/docs/3.4/css/" // test for now
-
 function ArticleViewer() {
   return (
-    <div id="parent">
-        <iframe src="{ articleURL }"></iframe>
+    <div className="ArticleViewer">
+        <iframe src="{ this.props.url }"></iframe>
     </div>
   );
 }
