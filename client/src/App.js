084e691ed560678bdc8a68ce6d6ecafb8e7b9000
TobBot2 <38795143+TobBot2@users.noreply.github.com>
2022-09-17 18:52:42 -0400

    adding firebase compatability

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,34 @@ import { BrowserRouter as Router, Routes, Route}
 import ArticleViewer  from './components/articleViewer/ArticleViewer';
 import ArticleSelect from './components/articleSelect/ArticleSelect';
 
+/************************
+ ****** FIREBASE ********
+ ************************/
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCKAN-t_Iju-z5VUnrIe8uxUe6K1mtJg40",
+  authDomain: "hophacksfall22.firebaseapp.com",
+  projectId: "hophacksfall22",
+  storageBucket: "hophacksfall22.appspot.com",
+  messagingSenderId: "63315149894",
+  appId: "1:63315149894:web:6a9417347182bfbf42f647",
+  measurementId: "G-BKN52TTLGJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// *************************
+// END FIREBASE
+// *************************
+
 function App() {
   return (
     <Router>
